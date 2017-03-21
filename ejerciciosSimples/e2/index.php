da19527e39861a37704747c9a8c2014038da3c0d
alumno <alumno>
2017-03-20 21:59:13 -0300

    agrego ejercicio 2

--- ejerciciosSimples/e2/index.php
+++ ejerciciosSimples/e2/index.php
@@ -1,33 +1,5 @@
 <?php 
-    //Se define a inicializa el array
-    //$miArray = array(rand(0, 10), rand(0, 10), rand(0, 10), rand(0, 10), rand(0, 10));
-    
-    //Se define el array y se le cargan valores indexandolo
-    $miArray = array();
-    // for($i = 0; $i < 5; $i++)
-    // {
-    //     $miArray[$i] = rand(0, 10);
-    // }
-
-    // Se carga el array con el meodo push
-    for($i = 0; $i < 5; $i++)
-    {
-        array_push($miArray, rand(0, 10));
-    }
-    
-    //var_dump($miArray);
-    $acumulador = 0;
-
-    foreach ($miArray as $value) {
-        $acumulador += $value;
-    }
-
-    $resultado = $acumulador / count($miArray);
-
-    if($resultado < 6)
-        echo "El promedio es menor a 6";
-    else if($resultado == 6)
-        echo "El promedio es igual a 6";
-    else
-        echo "El promedio es mayor a 6";
+    date_default_timezone_set("America/Buenos_Aires");
+    $fecha = date("d/m/Y   H:i:s");
+    echo $fecha . "<br>";
 ?>
