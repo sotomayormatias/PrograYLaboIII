a520faa86b25618165ffc8f274e5f64fe9c39e1f
alumno <alumno>
2017-03-20 21:25:15 -0300

    Se crea el array inicializandolo al momento de definirlo

--- index.php
+++ index.php
@@ -1,3 +1,19 @@
 <?php 
-    echo "Hola mundo";
+    $miArray = array(rand(0, 10), rand(0, 10), rand(0, 10), rand(0, 10), rand(0, 10));
+
+    // var_dump($miArray);
+    $acumulador = 0;
+
+    foreach ($miArray as $value) {
+        $acumulador += $value;
+    }
+
+    $resultado = $acumulador / count($miArray);
+
+    if($resultado < 6)
+        echo "El promedio es menor a 6";
+    else if($resultado == 6)
+        echo "El promedio es igual a 6";
+    else
+        echo "El promedio es mayor a 6";
 ?>
