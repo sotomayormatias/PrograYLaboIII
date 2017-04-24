1fc76452ee32fbb5861e89852dcd1932e21a60a7
alumno <alumno>
2017-04-24 19:15:45 -0300

    Subo template de BD

---
+++ TP05/utn.sql
@@ -0,0 +1,116 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 25-04-2017 a las 00:14:57
+-- Versión del servidor: 10.1.19-MariaDB
+-- Versión de PHP: 5.6.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `utn`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `envios`
+--
+
+CREATE TABLE `envios` (
+  `numero` int(11) NOT NULL,
+  `pNumero` int(11) NOT NULL,
+  `cantidad` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `envios`
+--
+
+INSERT INTO `envios` (`numero`, `pNumero`, `cantidad`) VALUES
+(100, 1, 500),
+(100, 2, 1500),
+(100, 3, 100),
+(101, 2, 55),
+(101, 3, 225),
+(102, 1, 600),
+(102, 3, 300);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `productos`
+--
+
+CREATE TABLE `productos` (
+  `pNumero` int(11) NOT NULL,
+  `pNombre` varchar(30) NOT NULL,
+  `precio` float NOT NULL,
+  `tamanio` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `productos`
+--
+
+INSERT INTO `productos` (`pNumero`, `pNombre`, `precio`, `tamanio`) VALUES
+(1, 'Caramelos', 1.5, 'Chico'),
+(2, 'Cigarrillos', 45.89, 'Mediano'),
+(3, 'Gaseosa', 15.8, 'Grande');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `proveedores`
+--
+
+CREATE TABLE `proveedores` (
+  `numero` int(11) NOT NULL,
+  `nombre` varchar(30) NOT NULL,
+  `domicilio` varchar(50) NOT NULL,
+  `localidad` varchar(80) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `proveedores`
+--
+
+INSERT INTO `proveedores` (`numero`, `nombre`, `domicilio`, `localidad`) VALUES
+(100, 'Perez', 'Perón 876', 'Quilmes'),
+(101, 'Gimenez', 'Mitre 750', 'Avellaneda'),
+(102, 'Aguirre', 'Boedo 634', 'Bernal');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `envios`
+--
+ALTER TABLE `envios`
+  ADD PRIMARY KEY (`numero`,`pNumero`);
+
+--
+-- Indices de la tabla `productos`
+--
+ALTER TABLE `productos`
+  ADD PRIMARY KEY (`pNumero`);
+
+--
+-- Indices de la tabla `proveedores`
+--
+ALTER TABLE `proveedores`
+  ADD PRIMARY KEY (`numero`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
